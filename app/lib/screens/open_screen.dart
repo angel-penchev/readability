f7aa526935ae51e78966cdf4b7a09b5366363c83
Angel Penchev <angel.penchev.tsb@gmail.com>
2019-04-06 13:34:46 +0300

    File structure fixes
    Removed unused files from lib/ in addition to reorganising the rest of the files.

---
+++ app/lib/screens/open_screen.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class OpenScreen extends StatefulWidget {
+  createState() => _OpenScreen();
+}
+
+class _OpenScreen extends State<OpenScreen> {
+  final PageController ctrl = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: PageView(
+          scrollDirection: Axis.vertical,
+          controller: ctrl,
+          children: [
+            FutureBuilder(
+              future: _getLocalFile("Pictures/1554055531209.jpg"),
+              builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+                return snapshot.data != null
+                    ? new Image.file(snapshot.data)
+                    : new Container();
+              },
+            ),
+            FutureBuilder(
+              future: _getLocalFile("Pictures/1554059005529.jpg"),
+              builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+                return snapshot.data != null
+                    ? new Image.file(snapshot.data)
+                    : new Container();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<File> _getLocalFile(String filename) async {
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    File f = new File('$dir/$filename');
+    return f;
+  }
+}
---
+++ app/lib/screens/settings_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Text(
+        "Settings",
+        style: new TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+}
